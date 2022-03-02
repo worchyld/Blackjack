2b0622f9315d493527606461cb62a9cf82528eae
AD <silentflute.ad@gmail.com>
2022-03-02 07:48:15 +0000

    Project creation
    Project creation

---
+++ Blackjack/Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Amarjit on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blackjack/Blackjack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blackjack
+//
+//  Created by Amarjit on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
