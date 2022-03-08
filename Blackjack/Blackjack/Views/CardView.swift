64aaa8f2d8143c50abde32dcbfa6cbc6af9c7cbc
AD <silentflute.ad@gmail.com>
2022-03-08 23:37:03 +0000

    removed unused card

--- Blackjack/Blackjack/Views/CardView.swift
+++ Blackjack/Blackjack/Views/CardView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct CardView: View {
-    let card = Card.init(rank: .queen, suit: .clubs)
-    
     var body: some View {
         Image("clubs-10").resizable().frame(width: 85, height: 135, alignment: .center)
     }
