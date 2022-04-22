d3d8466e060ef8909715a7193f657590675c1fd5
AD <silentflute.ad@gmail.com>
2022-04-22 04:13:06 +0100

    add a simple main menu

--- Blackjack/Blackjack/Views/MainMenuView.swift
+++ Blackjack/Blackjack/Views/MainMenuView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MainMenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Simple Blackjack")
+            Button {
+            } label: {
+                Text("Play")
+            }
+        }
     }
 }
 
