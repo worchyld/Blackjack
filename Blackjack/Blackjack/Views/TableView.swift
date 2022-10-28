a7ed2d510f116266c8b75f8ae8e3c01dffe10d3c
AD <silentflute.ad@gmail.com>
2022-10-28 18:07:59 +0100

    add hex helper, muddle something with view

--- Blackjack/Blackjack/Views/TableView.swift
+++ Blackjack/Blackjack/Views/TableView.swift
@@ -9,26 +9,32 @@ import SwiftUI
 
 struct TableView: View {
     var body: some View {
-        VStack {
-            HStack(alignment: .center, spacing: 20) {
-                Text("Score: 0")
-                Button {
-                } label: {
-                    Text("Menu")
-                }
-            }
-        
-            HStack(alignment: .center, spacing: 20) {
-                Button {
-                } label: {
-                    Text("Hit")
-                }
-                Button {
-                } label: {
-                    Text("Stand")
-                }
-            }
-        }
+        //50,182,122
+        Color(hex: "#32B67A")
+            .ignoresSafeArea(.all) // Ignore just for the color
+               .overlay(
+                   VStack(spacing: 20) {
+                       //Text("Overlay").font(.largeTitle)
+                       //Text("Example").font(.title).foregroundColor(.white)
+                       HStack(alignment: .center, spacing: 20) {
+                           Text("Score: 0")
+                           Button {
+                           } label: {
+                               Text("Menu").font(.largeTitle).foregroundColor(.white)
+                           }
+                       }
+                   
+                       HStack(alignment: .center, spacing: 20) {
+                           Button {
+                           } label: {
+                               Text("Hit")
+                           }
+                           Button {
+                           } label: {
+                               Text("Stand")
+                           }
+                       }
+               })
     }
 }
 
