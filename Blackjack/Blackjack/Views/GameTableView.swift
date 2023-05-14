b6069527e7be67d35f9eb7faf4fad939d27f477f
AD <silentflute.ad@gmail.com>
2023-05-14 19:05:34 +0100

    adding some basic swiftui to the table view

--- Blackjack/Blackjack/Views/GameTableView.swift
+++ Blackjack/Blackjack/Views/GameTableView.swift
@@ -9,10 +9,43 @@ import SwiftUI
 
 struct GameTableView: View {        
     var body: some View {
-        ZStack {
-            Text("Hello world")
-                .font(.body)
+        VStack {
+            Text("Simple BlackJack")
+                .font(.largeTitle)
                 .foregroundColor(.white)
+            
+            Text("Score: 10")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            HStack(alignment: .center, spacing: 30.0) {
+                Button(action: {
+                    print("Hello button tapped!")
+                }) {
+                    Text("Hit")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+                        )
+                }
+                
+                Button(action: {
+                    print("Hello button tapped!")
+                }) {
+                    Text("Stand")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+                        )
+                }
+            }.padding(20)
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.green)
