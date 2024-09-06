eb23c38da92d5c6f9d5b4458f9a277976a54bdd1
AD <silentflute.ad@gmail.com>
2024-09-06 18:49:52 +0100

    Add a main menu

--- Blackjack/Blackjack/ContentView.swift
+++ Blackjack/Blackjack/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Simple BlackJack")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                        
+            HStack(alignment: .center, spacing: 30.0) {
+                Button(action: {
+                    print("Hello button tapped!")
+                }) {
+                    Text("Play")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+                        )
+                }                
+            }.padding(20)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.green)
     }
 }
 
